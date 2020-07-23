1b0fc6ca39761cceffbcb24ee8b8a17269b4c64e
Nicole Apsche <napsche@kgtiger.com>
2020-07-22 20:13:21 -0400

    adding Mae's UI changes

---
+++ db/seeds.sql
@@ -0,0 +1,1 @@
+INSERT INTO gitpet_users (uname, email, pass) VALUES ('Mae Egner', 'hellogoodbyemae@gmail.com', 'b@il3y');
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Middleware
+//should extended be true or false? 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -25,23 +26,29 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: false };
+// var syncOptions = { force: false };
 
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
+// // If running a test, set syncOptions.force to true
+// // clearing the `testdb`
+// if (process.env.NODE_ENV === "test") {
+//   syncOptions.force = true;
+// }
 
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+// // Starting the server, syncing our models ------------------------------------/
+// db.sequelize.sync(syncOptions).then(function() {
+//   app.listen(PORT, function() {
+//     console.log(
+//       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+//       PORT,
+//       PORT
+//     );
+//   });
+// });
+
+// module.exports = app;
+
+app.listen(PORT, function() {
+  console.log("----------------------------------------------")
+  console.log("App listening on PORT " + PORT);
 });
-
-module.exports = app;
+// });
