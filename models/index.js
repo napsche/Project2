adef0772cb5b9063da5b205296fbf1034bc09f01
Margaret Egner <hellogoodbyemae@gmail.com>
2020-07-25 11:00:55 -0400

    rebase

--- models/index.js
+++ models/index.js
@@ -27,4 +27,5 @@ Object.keys(db).forEach(function(modelName) {
 });
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
 module.exports = db;
---
+++ public/assets/js/login.js
@@ -0,0 +1,2 @@
+//has to link to login modal
+//needs logic to loop through users for matching credentials.
