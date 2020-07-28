358c5261e99ddad804163e6febcebdceb707b89d
Nicole Apsche <napsche@kgtiger.com>
2020-07-28 19:22:06 -0400

    edit search.js and apiroutes; now saves to database AND displays in console

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -5,79 +5,63 @@ var pf = new petfinder.Client({
   secret: "ImWvGTeVwcchWGAuI9Cumt06OSznXlL7uPoX65j9"
 });
 
-
-function submitSearch(newSearch) {
-  console.log("making it!");
-  // var holdingArr = ["pet", "location", "gender", "age"];
+// this works and posts results to the db
+function postFxn() {
   event.preventDefault();
+  var pet = $("#pet").val();
+  var location = $("#location").val();
+  var gender = $("#gender").val();
+  var age = $("#age").val();
 
-  pf.animal.search().then(function (response) {
+  $.post("/api/search", { pet, location, gender, age }, function (response) {
     event.preventDefault();
-    console.log("you're inside!");
     console.log(response);
-
-    
-    var pet = $("#pet").val();
-    var location = $("#location").val();
-    var gender = $("#gender").val();
-    var age = $("#age").val();
-
-    $.post("/api/search", { pet, location, gender, age }, function (response) {
-      event.preventDefault();
-      console.log(response);
-      response = JSON.stringify(response);
-      localStorage.setItem("animal", response);
-      window.location.replace("index.html");
-      console.log("alllll the way here");
-    });
-
-    // for (var i = 0; i < .length; i++) {
-    //   console.log(response.results[i].animal.type);
-    //   console.log(response.results[i].animal.contact.location);
-    //   console.log(response.results[i].animal.gender);
-    //   console.log(response.results[i].animal.age);
-    //   console.log("even further");
-
-    //   animalDiv = $("#search-container");
-
-    //   var type = response.results[i].animal.type;
-    //   var pOne = $("<h4>").text(type);
-    //   animalDiv.append(pOne);
-
-    //   var location = response.results[i].animal.contact.location;
-    //   var pTwo = $("<h4>").text(location);
-    //   animalDiv.append(pTwo);
-
-    //   var gender = response.results[i].animal.gender;
-    //   var pThree = $("<h4>").text(gender);
-    //   animalDiv.append(pThree);
-
-    //   var age = response.results[i].animal.age;
-    //   var pFour = $("<h4>").text(age);
-    //   animalDiv.append(pFour);
-
-    //   $("#search-container").prepend(animalDiv);
-    // }
+    response = JSON.stringify(response);
+    localStorage.setItem("animal", response);
+    // window.location.replace("/");
+    console.log("alllll the way here");
   });
-  // console.log(newSearch);
-  // // window.location.href=`/search?q=${search}`;
-  // var foo = $(this); 
-  // console.log(foo);
-
-  // foo = JSON.stringify(holdingArr);
-  // var pet = $("#pet").val();
-  // var location = $("#location").val();
-  // var gender = $("#gender").val();
-  // var age = $("#age").val();
-
-  // $.post("/api/search", { pet, location, gender, age }, function(response) {
-  //   console.log(response);
-  //   response = JSON.stringify(response);
-  //   localStorage.setItem("animal", response);
-  //   window.location.replace("index.html"); 
-  //   console.log("alllll the way here");
-  // });
 }
+
+  function submitSearch(newSearch) {
+    console.log("making it!");
+    event.preventDefault();
+  
+    pf.animal.search(newSearch).then(function (response) {
+      event.preventDefault();
+      console.log("you're inside!");
+      postFxn();
+     
+      // for (var i = 0; i < .length; i++) {
+      //   console.log(response.results[i].animal.type);
+      //   console.log(response.results[i].animal.contact.location);
+      //   console.log(response.results[i].animal.gender);
+      //   console.log(response.results[i].animal.age);
+      //   console.log("even further");
+  
+      //   animalDiv = $("#search-container");
+  
+      //   var type = response.results[i].animal.type;
+      //   var pOne = $("<h4>").text(type);
+      //   animalDiv.append(pOne);
+  
+      //   var location = response.results[i].animal.contact.location;
+      //   var pTwo = $("<h4>").text(location);
+      //   animalDiv.append(pTwo);
+  
+      //   var gender = response.results[i].animal.gender;
+      //   var pThree = $("<h4>").text(gender);
+      //   animalDiv.append(pThree);
+  
+      //   var age = response.results[i].animal.age;
+      //   var pFour = $("<h4>").text(age);
+      //   animalDiv.append(pFour);
+  
+      //   $("#search-container").prepend(animalDiv);
+      // }
+    });
+  }
+
 
 $("#search-form").on("submit", function (event) {
   event.preventDefault();
