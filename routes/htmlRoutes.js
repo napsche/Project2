4f740be44dfc428d86323fe28c4f5f5bbeaac1be
Nicole Apsche <napsche@kgtiger.com>
2020-07-29 22:37:58 -0400

    adding passport

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,9 @@ var path = require("path");
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
+    if (req.user) {
+      res.redirect("/");
+    }
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
