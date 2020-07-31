7c3038540fdaec306b4ab76857afa97d06a72c65
Margaret Egner <hellogoodbyemae@gmail.com>
2020-07-31 15:56:30 -0400

    figuring out link button on search.js

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -30,36 +30,9 @@ function postFxn() {
       event.preventDefault();
       console.log("you're inside!");
       postFxn();
-     
-      // for (var i = 0; i < .length; i++) {
-      //   console.log(response.results[i].animal.type);
-      //   console.log(response.results[i].animal.contact.location);
-      //   console.log(response.results[i].animal.gender);
-      //   console.log(response.results[i].animal.age);
-      //   console.log("even further");
-  
-      //   animalDiv = $("#search-container");
-  
-      //   var type = response.results[i].animal.type;
-      //   var pOne = $("<h4>").text(type);
-      //   animalDiv.append(pOne);
-  
-      //   var location = response.results[i].animal.contact.location;
-      //   var pTwo = $("<h4>").text(location);
-      //   animalDiv.append(pTwo);
-  
-      //   var gender = response.results[i].animal.gender;
-      //   var pThree = $("<h4>").text(gender);
-      //   animalDiv.append(pThree);
-  
-      //   var age = response.results[i].animal.age;
-      //   var pFour = $("<h4>").text(age);
-      //   animalDiv.append(pFour);
-  
-      //   $("#search-container").prepend(animalDiv);
-      // }
     });
   }
+  
   $("#search-form").on("submit", function (event) {
     event.preventDefault();
     console.log("here!!");
@@ -93,11 +66,17 @@ function postFxn() {
         var petSize = response.data.animals[i].size;
         var petDescription = response.data.animals[i].description;
         var petLink = response.data.animals[i].url;
-        var likeBtn = $("<button>");
-        likeBtn.text("");
-        likeBtn.addClass("<fas fa-thumbs-up>");
+        var linkBtn = $("<button>");
+        linkBtn.text(petName);
+        linkBtn.addClass("btn-pet");
+        linkBtn.data('pet-url', petLink);
+
+        $('body').on('click', '.btn-pet', function(){
+          var link = $(this).data('pet-url');  
+          // window.location.href = link;
+        })
         
-        $("#tBody").append("<tr><td>"+petName+"</td>"+"<td>"+"<img src='"+petPicture+"' class='img-fluid img-thumbnail'>"+"</td>"+"<td>"+petSize+"</td>"+"</td>"+"<td>"+petDescription+"</td>"+"</td>"+"<td>"+petLink+"</td>"+"</td>"+"<td>"+likeBtn+"</td>"+"</tr>")
+        $("#tBody").append("<tr><td>"+petName+"</td>"+"<td>"+"<img src='"+petPicture+"' class='img-fluid img-thumbnail'>"+"</td>"+"<td>"+petSize+"</td>"+"</td>"+"<td>"+petDescription+"</td>"+"</td>"+"<td>"+linkBtn+"</td>"+"</tr>")
        }
 
         // var results = JSON.stringify(response);
